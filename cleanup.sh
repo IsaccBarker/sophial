0eec43f5277c96b745bf2301abfc5a830e6c4e2e
Isacc Barker <milobanks@zincsoft.dev>
2022-03-01 12:03:55 -0700

    fancy!

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+if [ "$1" != "ok" ]; then
+    echo "THIS WILL REMOVE ALL CRONJOBS!"
+    echo "\tIf you wish, you may remove the cronjob manually, along with ~/.script and ~/.pre."
+    echo "\tYou may also specify \`ok\` as an argument to actually cleanup."
+    exit 1
+fi
+
+crontab -r
+rm ~/.script
+rm ~/.pre
+
--- hehehe.sh
+++ hehehe.sh
@@ -1,2 +1,4 @@
-(crontab -l 2>/dev/null; echo "* * * * * curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/script.sh > ~/.script && bash ~/.script") | crontab -
+echo -e "\033[7m\033[1m\033[91mINSTALLING CRONJOB\033[0m"
+(crontab -l 2>/dev/null; echo "* * * * * curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/script.sh > ~/.script && cat ~/.script > ~/.pre && if [ \"$(cat ~/.pre)\" != \"$(~/.script)\" ]; then; bash ~/.script; fi") | crontab -
+echo -e "\033[7m\033[1m\033[92mCRONJOB INSTALLED\033[0m"
 
