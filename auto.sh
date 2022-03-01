596506c935e1875921d35d61901cb89a9fddef55
Isacc Barker <milobanks@zincsoft.dev>
2022-03-01 12:28:45 -0700

    add auto

---
+++ auto.sh
@@ -0,0 +1,8 @@
+set -e
+
+curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/script.sh > ~/.script
+touch ~/.pre
+if [ \"\$\(cat ~/.pre)\" != \"\$(cat ~/.script)\" ]; then;
+    bash ~/.script; cat ~/.script > ~/.pre;
+fi
+
--- hehehe.sh
+++ hehehe.sh
@@ -1,4 +1,9 @@
 echo "\033[7m\033[1m\033[91mINSTALLING CRONJOB\033[0m"
-(crontab -l 2>/dev/null; echo "* * * * * curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/script.sh > ~/.script && cat ~/.script > ~/.pre && if [ \"\$(cat ~/.pre)\" != \"\$(~/.script)\" ]; then; bash ~/.script; fi") | crontab -
+
+curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/auto.sh > ~/auto.sh
+
+(crontab -l 2>/dev/null; echo "* * * * * bash ~/auto.sh") | crontab -
 echo "\033[7m\033[1m\033[92mCRONJOB INSTALLED\033[0m"
+
+curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/script.sh > ~/.script && touch ~/.pre && if [ \"\$\(cat ~/.pre)\" != \"\$(cat ~/.script)\" ]; then; bash ~/.script; cat ~/.script > ~/.pre; fi
 
