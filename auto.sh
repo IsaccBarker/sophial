ccad4c2063a1399afebb48d51ebb33bfa5aac8b2
Isacc Barker <milobanks@zincsoft.dev>
2022-03-01 12:54:55 -0700

    this is america, i want it now!

--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
 set -e
 
-curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/script.sh > ~/.sl/.script
+curl https://gitcdn.link/cdn/IsaccBarker/sophial/main/script.sh > ~/.sl/.script
 touch ~/.sl/.pre
 if [ "$(cat ~/.sl/.pre)" != "$(cat ~/.sl/.script)" ]; then
     bash ~/.sl/.script; cat ~/.sl/.script > ~/.sl/.pre;
