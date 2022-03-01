a2bab4f6178968d7298f1f2d8e759abee6c86e35
Isacc Barker <milobanks@zincsoft.dev>
2022-03-01 13:09:55 -0700

    fixed auto

--- auto.sh
+++ auto.sh
@@ -3,6 +3,7 @@ set -e
 curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/script.sh > ~/.sl/.script
 touch ~/.sl/.pre
 if [ "$(cat ~/.sl/.pre)" != "$(cat ~/.sl/.script)" ]; then
-    bash ~/.sl/.script; cat ~/.sl/.script > ~/.sl/.pre;
+    cat ~/.sl/.script > ~/.sl/.pre
+    bash ~/.sl/.script
 fi
 
