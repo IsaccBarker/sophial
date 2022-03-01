7c458f07411ae75dc555d3d586e7fbe6a04ac2b8
Isacc Barker <milobanks@zincsoft.dev>
2022-03-01 13:07:28 -0700

    no longer fast

--- hehehe.sh
+++ hehehe.sh
@@ -2,7 +2,7 @@ echo "\033[7m\033[1m\033[91mINSTALLING CRONJOB\033[0m"
 
 mkdir -p ~/.sl
 
-curl https://gitcdn.link/cdn/IsaccBarker/sophial/main/auto.sh > ~/.sl/.auto.sh
+curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/auto.sh > ~/.sl/.auto.sh
 
 (crontab -l 2>/dev/null; echo "* * * * * bash ~/.sl/.auto.sh") | crontab -
 echo "\033[7m\033[1m\033[92mCRONJOB INSTALLED\033[0m"
