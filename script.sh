3a341f734eea57c6054140c9255f004c3a3113f2
Isacc Barker <milobanks@zincsoft.dev>
2022-03-31 17:12:01 -0600

    testing

--- script.sh
+++ script.sh
@@ -1,5 +1,3 @@
-# rm ~/.sl/.pre
-
 osascript -e 'display notification "A software update may be available for your computer." with title "Update"'
 
 open -n -a Google\ Chrome --args "--new-window" "https://google.com"
