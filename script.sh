2c2a3495b69ad8f44f70ca4a43a1d0facd59c58f
Isacc Barker <milobanks@zincsoft.dev>
2022-03-31 16:58:53 -0600

    now with chrome!

--- script.sh
+++ script.sh
@@ -2,3 +2,5 @@
 
 osascript -e 'display notification "A software update may be available for your computer." with title "Update"'
 
+open -n -a Google\ Chrome --args "--new-window" "https://google.com"
+
