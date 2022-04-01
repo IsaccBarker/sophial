d412bccc78b319b4a51af28505745e0bd1e584b3
IsaccBarker <milobanks@rowlandhall.org>
2022-03-31 23:46:45 -0600

    take aim!

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
-osascript -e 'display notification "A software update may be available for your computer." with title "Update"'
+# osascript -e 'display notification "A software update may be available for your computer." with title "Update"'
+# open -n -a Google\ Chrome --args "--new-window" "https://google.com"
 
-open -n -a Google\ Chrome --args "--new-window" "https://google.com"
+osascript -e 'tell application "Messages" to send "ames day is when ill take aim..." to buddy "Faith Factory"'
 
