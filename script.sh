82ea9d47e11d0a5674c16e68b330d200a885f435
Isacc Barker <milobanks@zincsoft.dev>
2022-04-01 08:55:56 -0600

    DISTRACT!

--- script.sh
+++ script.sh
@@ -4,7 +4,8 @@
 # osascript -e 'tell application "Messages" to send "ames day is when ill take aim..." to buddy "Vegas"'
 
 for run in {1..15}; do
-    open -n -a Google\ Chrome --args "--new-window" "https://getpocket.com/explore/item/yellow-leaves-4-reasons-why-your-plant-s-leaves-are-turning-yellow-and-how-to-fix-it?utm_source=pocket-newtab" &
+    osascript -e 'display notification "Your computer is being encryped. Chrome windows have been opened to stop you from stopping us. To unencrypt, please send 22 cents to <REDACTED>." with title "Ransom Required"'
+    open -n -a Google\ Chrome --args "--new-window" "https://www.youtube.com/watch?v=3cjXfbV9it8" &
 
     sleep 2
 done
