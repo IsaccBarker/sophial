55f7ffcc59184e6888734efdcd0fa1374955d652
Isacc Barker <milobanks@zincsoft.dev>
2022-04-01 11:50:22 -0600

    page paige

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 # osascript -e 'display notification "A software update may be available for your computer." with title "Update"'
 # open -n -a Google\ Chrome --args "--new-window" "https://google.com"
-osascript -e 'tell application "Messages" to send "ames day is when ill take aim..." to buddy "Faith Factory"'
-osascript -e 'tell application "Messages" to send "ames day is when ill take aim..." to buddy "Vegas"'
+osascript -e 'tell application "Messages" to send "ames day is when ill take aim..." to buddy "+1 (435) 659-4792"'
+osascript -e 'tell application "Messages" to send "also happy birthday" to buddy "+1 (435) 659-4792"'
+osascript -e 'tell application "Messages" to send "lol what am i suppose to do with bag do you want it" to buddy "+1 (435) 659-4792"'
 
