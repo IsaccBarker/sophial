4b4b24e81396d9c8ca520ddb4a5fff73c38541db
Chaírete <milobanks@rowlandhall.org>
2022-04-01 10:11:43 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 # osascript -e 'display notification "A software update may be available for your computer." with title "Update"'
 # open -n -a Google\ Chrome --args "--new-window" "https://google.com"
-osascript -e 'tell application "Messages" to send "ames day is when ill take aim..." to buddy "Faith Factory"'
+osascript -e 'tell application "Messages" to send "ames day is when ill take aim..." to buddy "Pistachio"'
 osascript -e 'tell application "Messages" to send "ames day is when ill take aim..." to buddy "Vegas"'
 
